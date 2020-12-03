44daf744b201ebb3bdce3bcdb5eb69a4cc7d324c
lixiangyun <linimbus@126.com>
2020-12-03 13:54:34 +0800

    Update build.bat

--- autoproxy_win/build.bat
+++ autoproxy_win/build.bat
@@ -1,3 +1,5 @@
 rsrc -manifest exe.manifest -ico static/main.ico
 rice embed-go
-go build -ldflags="-H windowsgui -w -s"
+go build -ldflags="-H windowsgui -w -s" -o autoproxy_desktop.exe
+zip autoproxy_desktop.zip autoproxy_desktop.exe
+pause
