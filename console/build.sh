a4c761359ed461ff4b2f875b2a910b5c08e680d9
lixiangyun <linimbus@126.com>
2021-04-19 23:03:50 +0800

    Update build.sh

--- console/build.sh
+++ console/build.sh
@@ -2,4 +2,4 @@ export CGO_ENABLED=1
 export GARCH=$(go env GOARCH)
 
 go build -ldflags="-w -s" -o proxyweb
-tar -zcf proxyweb_linux_$GARCH.tar.gz.tar.gz proxyweb release.db config.json
+tar -zcf proxyweb_linux_$GARCH.tar.gz proxyweb release.db config.json
